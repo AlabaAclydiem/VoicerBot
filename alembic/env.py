74f79b0a7271a54f0e0f6630a0f1df34402144be
AlabaAclydiem <pastukhou.kirill@gmail.com>
2024-04-28 13:02:38 +0300

    return alembic.ini + no hardcoded DB URL

--- alembic/env.py
+++ alembic/env.py
@@ -4,12 +4,13 @@ from logging.config import fileConfig
 from sqlalchemy import pool
 from sqlalchemy.engine import Connection
 from sqlalchemy.ext.asyncio import async_engine_from_config
-
+from settings import settings
 from alembic import context
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
+config.set_main_option("sqlalchemy.url", settings.DATABASE_URL)
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
