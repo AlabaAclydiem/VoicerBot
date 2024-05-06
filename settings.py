8c37b05ce0a2a9730100dce97ef74b87e3527616
AlabaAclydiem <pastukhou.kirill@gmail.com>
2024-05-06 17:42:10 +0300

    settings update

--- settings.py
+++ settings.py
@@ -12,15 +12,29 @@ class Settings(BaseSettings):
     BOT_TOKEN: str = Field(env='BOT_TOKEN')
     DATABASE_URL: str = Field(env='DATABASE_URL')
     AMPLITUDE_API: str = Field(env='AMPLITUDE_API')
+    REDISHOST: str = Field(env='REDISHOST')
+    REDISPASSWORD: str = Field(env='REDISPASSWORD')
+    REDISUSER: str = Field(env='REDISUSER')
+    REDISPORT: str = Field(env='REDISPORT')
 
     class Config:
         env_file = '.env'
         env_file_encoding = 'utf-8'
 
 settings = Settings()
+if settings.REDISPASSWORD == 'None':
+    settings.REDISPASSWORD = None
+if settings.REDISUSER == 'None':
+    settings.REDISUSER = None
+if settings.REDISPORT == 'None':
+    settings.REDISPORT = None
 
 voicer_bot = Bot(token=settings.BOT_TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
-redis_client = Redis(host='redis')
+redis_client = Redis(
+    host=settings.REDISHOST, 
+    username=settings.REDISUSER, 
+    password=settings.REDISPASSWORD,
+    port=settings.REDISPORT)
 dispatcher = Dispatcher(storage=RedisStorage(redis=redis_client))
 
 executor = ThreadPoolExecutor(max_workers=10)
